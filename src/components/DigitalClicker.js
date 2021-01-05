4f2270b28139597d38842f41cf28911d51dcb917
Bakhos Rachwan <bakhos.rachwan@gmail.com>
2021-01-05 22:41:53 +0000

    Automatically backed up by Learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -3,12 +3,12 @@ import React from 'react'
 class GigitalClicker extends React.Component{
   constructor(){
     super();
-    
+
     this.state {
       timesClicked : 0
     }
   }
-  
+
   render(){
     return(
       <button>{this.state.timesClicked}</button>
