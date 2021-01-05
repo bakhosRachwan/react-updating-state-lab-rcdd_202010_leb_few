8415f281945d51283a46fb6e5426d8cb9f2f4b18
Bakhos Rachwan <bakhos.rachwan@gmail.com>
2021-01-05 22:34:17 +0000

    Automatically backed up by Learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,12 @@
-// Code DigitalClicker Component Here
+import React from 'react'
+
+class GigitalClicker extends React.Component{
+  this.state {
+    timesClicked : 0
+  }
+  render(){
+    return(
+      <button>{this.state.timesClicked}</button>
+    )
+  }
+}
